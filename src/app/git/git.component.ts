e59315194e2c26f0ffd518e52a45ea53f7df50de
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-06-03 09:57:18 +0200

    df

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -7,15 +7,13 @@ import { Component } from '@angular/core';
 })
 export class GitComponent {
 
-<<<<<<< HEAD
+
   beforeAmend:string = 'false'
   beforeAmend2:string = 'false';
 
   cherryPick: string = '8:56';
-=======
   resA : number = 0;
   resB: number = 1;
->>>>>>> 905ea74153f152e64571137c8ad471232000437c
 
 
 }
