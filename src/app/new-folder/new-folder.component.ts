596b10c8dc049ffcf7ddd879c87d786636da69d9
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-09-24 14:06:52 +0200

    nf

---
+++ src/app/new-folder/new-folder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-folder',
+  templateUrl: './new-folder.component.html',
+  styleUrls: ['./new-folder.component.scss']
+})
+export class NewFolderComponent {
+
+}
