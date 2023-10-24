bc2afeb3e80ed9a8fc294ffb878441efc7fff5c6
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-10-24 20:55:50 +0200

    new debugger

--- src/app/debug/debug.component.ts
+++ src/app/debug/debug.component.ts
@@ -11,33 +11,11 @@ export class DebugComponent implements OnInit {
   constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.dateFn()
+    // this.dateFn()
   }
 
-  dateFn() {
-    let startDate: Date = new Date();
-    let currentDate: Date = new Date (startDate)
-    let endDate = new Date('November 12, 2026')
-    let valueOfFrequency = 1;
-    let arr: any = [
-      new Date()
-    ];
 
-    let handler = () => {
-      if (currentDate < endDate) {
-      let  newCurrentDate = new Date(currentDate.setMonth(currentDate.getMonth() + 4 * valueOfFrequency));
-
-        if (newCurrentDate < endDate) {
-          arr.push((newCurrentDate));
-        }
-        handler()
-      } else return
-    }
-
-    handler()
-    console.log(arr);
-    // console.log(new Date().toLocaleTimeString('en-US'))
-
+    
   }
 
 
