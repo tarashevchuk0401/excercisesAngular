6a9f8a7513ee807d615ba6e9ec23a7f641eb7667
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-12-02 14:31:08 +0100

    combine latest

--- src/app/indexed-db/indexed-db.component.ts
+++ src/app/indexed-db/indexed-db.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IndexeddbService } from '../services/indexeddb.service';
+import { combineAll, combineLatest, combineLatestAll, interval, of } from 'rxjs';
 
 @Component({
   selector: 'app-indexed-db',
@@ -11,11 +12,39 @@ export class IndexedDBComponent implements OnInit{
   constructor(private indexedDB: IndexeddbService){}
 
   ngOnInit(): void {
+
+
     let data = {"test": 222}
       this.indexedDB.openDB().subscribe()
-      this.indexedDB.addData(data).subscribe(d => console.log('added'))
+      this.indexedDB.addData(data).subscribe()
 
-      this.indexedDB.readData().subscribe(d => console.log(d))
+      this.indexedDB.readData().subscribe();
+
+      this.combine()
+  }
+
+  combine(){
+    let aaa =of('a', 'b', 'c');
+    let bbb = of(1,2,3);
+
+  
+
+    combineLatest<[string, number]>(aaa, bbb).subscribe(d => console.log(d))
+  }
+
+  arr(){
+    
+    let arr = new Array();
+    arr.push(221);
+
+    const getNum = (item: number | undefined) => {
+      if(item){
+        console.log(item.toString())
+
+      }
+    }
+
+    getNum(undefined)
   }
 
 }
